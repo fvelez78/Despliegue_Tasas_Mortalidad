1ad84a3e2307335bbd832f3eb80cb4336221be6c
fvelez78 <fvelez78@yahoo.es>
2025-06-27 09:10:21 -0500

    Ajuste extension

--- Home_Tablero.py
+++ Home_Tablero.py
@@ -41,7 +41,7 @@ with open('style.css')as f:
 # cargar archivo Excel | comente esta línea cuando obtenga datos de MySQL:
 
 # df = pd.read_excel("C:/Users/cesar/Downloads/TABLERO_STREAMLIT_DASHBOARD/DASHBOARD_Morbilidad_DESPLIEGUE/Tasas_Morbilidad.xlsx", sheet_name='Hoja1')
-df = pd.read_csv('Tasas_Mortalidad2.xlsx')
+df = pd.read_csv('Tasas_Mortalidad2.csv')
 
 # Convirtiendo la columna Anio a Categórica:
     # Opción 2: Convertir a categórica (más eficiente)
